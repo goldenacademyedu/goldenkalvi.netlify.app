ea1cbcb8440ea2602119b08c74cdf7832f73aec6
goldenacademyedu <goldenacademyedu@gmail.com>
2026-05-13 12:58:21 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCX4cWpNiisavu8maI3E-cQfkSt_9QxYhQ",
+  authDomain: "golden-academy-24509.firebaseapp.com",
+  projectId: "golden-academy-24509",
+  storageBucket: "golden-academy-24509.firebasestorage.app",
+  messagingSenderId: "11755509981",
+  appId: "1:11755509981:web:9579d7637c15867b786334",
+  measurementId: "G-2CNR8YXTMM"
+};
