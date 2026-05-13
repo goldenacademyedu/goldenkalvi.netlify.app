c8147d8f8f12428ec6347aadd18a733488de4d9b
goldenacademyedu <goldenacademyedu@gmail.com>
2026-05-13 13:01:22 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCX4cWpNiisavu8maI3E-cQfkSt_9QxYhQ",
   authDomain: "golden-academy-24509.firebaseapp.com",
@@ -7,3 +11,7 @@ const firebaseConfig = {
   appId: "1:11755509981:web:9579d7637c15867b786334",
   measurementId: "G-2CNR8YXTMM"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
